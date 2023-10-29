2ec03adfb8826647c163e9a27cf7f57d4c4ef8f3
joshpaulie <joshpaullee@gmail.com>
2023-10-29 18:30:51 -0500

    Finished calendar, started today

--- calendar_events.py
+++ calendar_events.py
@@ -1,36 +1,68 @@
+"""
+Many helpers to support the seemingly simple calendar commands
+"""
 import datetime as dt
 from operator import itemgetter
 
-# Constants
-today = dt.date.today()
-current_year = today.year
 
-# Events
-BIRTHDAYS = {
-    "Hudson": dt.date(current_year, 2, 14),
-    "Chris": dt.date(current_year, 4, 21),
-    "Alex": dt.date(current_year, 4, 15),
-    # 💔
-    # "Daniel": dt.date(current_year, 4, 3),
-    # "Lex": dt.date(current_year, 5, 20),
-    "Justin": dt.date(current_year, 6, 12),
-    "Josh": dt.date(current_year, 6, 27),
-    "Matt": dt.date(current_year, 9, 24),
-    "Hobo": dt.date(current_year, 9, 11),
-    "Conrad": dt.date(current_year, 10, 2),
-    "Vynle": dt.date(current_year, 5, 9),
-}
-BIRTHDAYS = {
-    f"""{goon}{"'" if goon[-1] == "s" else "'s"} birthday""" + " 🧁": date for goon, date in BIRTHDAYS.items()
-}
+def get_events_dict(today: dt.date) -> dict[str, dt.date]:
+    current_year = today.year
+    BIRTHDAYS = {
+        "Hudson": dt.date(current_year, 2, 14),
+        "Chris": dt.date(current_year, 4, 21),
+        "Alex": dt.date(current_year, 4, 15),
+        "Vynle": dt.date(current_year, 5, 9),
+        "Justin": dt.date(current_year, 6, 12),
+        "Josh": dt.date(current_year, 6, 27),
+        "Matt": dt.date(current_year, 9, 24),
+        "Hobo": dt.date(current_year, 9, 11),
+        "Conrad": dt.date(current_year, 10, 2),
+    }
+    BIRTHDAYS = {
+        f"""{goon}{"'" if goon[-1] == "s" else "'s"} birthday""" + " 🧁": date
+        for goon, date in BIRTHDAYS.items()
+    }
 
-HOLIDAYS = {
-    "Valentine's Day 💕": dt.date(current_year, 2, 14),
-    "Freedom Day 🎇": dt.date(current_year, 7, 6),
-    "Thanksgiving 🦃": dt.date(current_year, 11, 24),
-    "Christmas 🎄": dt.date(current_year, 12, 25),
-    "New Year's Eve 🥳": dt.date(current_year, 12, 31),
-}
+    HOLIDAYS = {
+        "Valentine's Day 💕": dt.date(current_year, 2, 14),
+        "Freedom Day 🎇": dt.date(current_year, 7, 6),
+        "Thanksgiving 🦃": dt.date(current_year, 11, 24),
+        "Christmas 🎄": dt.date(current_year, 12, 25),
+        "New Year's Eve 🥳": dt.date(current_year, 12, 31),
+    }
 
-CALENDAR_EVENTS = {**BIRTHDAYS, **HOLIDAYS}
-SORTED_CALENDAR_EVENTS = dict(sorted(CALENDAR_EVENTS.items(), key=itemgetter(1)))
+    # Combine
+    CALENDAR_EVENTS = {**BIRTHDAYS, **HOLIDAYS}
+    # Sort by date
+    SORTED_CALENDAR_EVENTS = dict(sorted(CALENDAR_EVENTS.items(), key=itemgetter(1)))
+    return SORTED_CALENDAR_EVENTS
+
+
+class SpecialEvent:
+    def __init__(self, today: dt.date, name: str, date: dt.date) -> None:
+        self.today = today
+        self.name = name
+        self.date = date
+
+    @property
+    def is_today(self):
+        return self.today == self.date
+
+    @property
+    def days_until(self):
+        return (self.date - self.today).days
+
+
+def get_events(today: dt.date, remaining_only: bool):
+    events = [
+        SpecialEvent(today, event_name, event_date)
+        for event_name, event_date in get_events_dict(today).items()
+    ]
+    if remaining_only:
+        return [e for e in events if e.days_until > -1]
+    return events
+
+
+# Birthday graveyard 💔
+# "Daniel": dt.date(current_year, 4, 3),
+# "Lex": dt.date(current_year, 5, 20),
--- cogs/goon_calendar.py
+++ cogs/goon_calendar.py
@@ -5,7 +5,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-import calendar_events
+from calendar_events import get_events
 from goonbot import Goonbot
 
 
@@ -17,20 +17,30 @@ class GoonCalendar(commands.Cog):
     @app_commands.describe(show_remaining_only="Show only remaining events for this year??")
     async def calendar(self, interaction: discord.Interaction, show_remaining_only: bool = True):
         """Take a peak at the Goon Calendar"""
+        today = dt.date.today()
+        # Create embed
         calendar_embed = self.bot.embed(title="Goon Calendar 📅")
-
-        events = calendar_events.SORTED_CALENDAR_EVENTS
         if show_remaining_only:
-            calendar_embed.description = "Events remaining for the year!"
-            events = {event: date for event, date in events.items() if date > dt.date.today()}
-
-        for event, event_date in events.items():
+            calendar_embed.description = f"Events remaining for {today.year}!"
+        # Add events
+        for event in get_events(today, show_remaining_only):
             calendar_embed.add_field(
-                name=event,
-                value=event_date.strftime("%B %d (%a)"),
-                inline=False,
+                name=event.name,
+                value=event.date.strftime("%B %d (%a)"),
             )
+        # Send it
         await interaction.response.send_message(embed=calendar_embed)
+
+    @app_commands.command(name="today")
+    async def today(self, interaction: discord.Interaction):
+        today = dt.date.today()
+        today_embed = self.bot.embed()
+        events = get_events(today, remaining_only=True)
+        todays_events = [e for e in events if e.is_today]
+        if todays_events:
+            todays_events_str = " and ".join(e.name for e in todays_events if e.is_today)
+            today_embed.title = f"Today is {todays_events_str}!"
+            await interaction.response.send_message(embed=today_embed)
 
 
 async def setup(bot):
