f34abb474c1d6dedbcaf262b6503a790bc6a6576
JoshPaulie (Bexli) <30572280+JoshPaulie@users.noreply.github.com>
2023-11-07 17:37:11 -0600

    Start /rat

---
+++ bex_tools.py
@@ -0,0 +1,34 @@
+"""Collection of utility objects"""
+
+
+import random
+from typing import Any, Sequence
+
+
+def cycle_random(items: Sequence[Any]):
+    """
+    Generates an infinite sequence of items from the given sequence, ensuring that each item is yielded before repeating any.
+    The order of the items is randomized each time the sequence cycles. Functionally similar to itertools.cycle()
+
+    Example:
+        >>> colors = ["Red", "Blue", "Yellow"]
+        >>> generator = cycle_random(colors)
+        >>> next(generator)
+        'Blue'
+        >>> next(generator)
+        'Red'
+        >>> next(generator)
+        'Yellow'
+        >>> next(generator)
+        'Red'
+        >>> next(generator)
+        'Yellow'
+        ...
+    """
+    used_items = []
+    while True:
+        if len(used_items) == len(items):
+            used_items = []
+        chosen_item = random.choice([i for i in items if i not in used_items])
+        used_items.append(chosen_item)
+        yield chosen_item
