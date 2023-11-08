9ca0013305380f58caea3b5799a00756f1a593ef
joshpaulie <joshpaullee@gmail.com>
2023-11-07 20:30:50 -0600

    Move report rat feat to message context menu

--- cogs/rats.py
+++ cogs/rats.py
@@ -1,7 +1,4 @@
-import logging
 import pathlib
-import random
-from typing import Optional
 
 import discord
 from discord import app_commands
@@ -11,74 +8,32 @@ from bex_tools import cycle_random
 from goonbot import Goonbot
 
 
-class ReportRat(discord.ui.View):
-    """Allows users to notify me of "broken rats" (images don't load for whatever reason)"""
-
-    def __init__(self, rat: str, bot: Goonbot):
-        super().__init__(timeout=12)
-        self.rat = rat
-        self.bot = bot
-        self.is_broken_rat = False
-
-    async def on_timeout(self) -> None:
-        self.stop()
-
-    @discord.ui.button(label="Pet Rat", emoji="👋", style=discord.ButtonStyle.blurple)
-    async def pet_rat(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message(
-            embed=self.bot.embed(
-                title="🐀👋",
-                url=self.rat,
-            ),
-            ephemeral=True,
-        )
-        self.stop()
-
-    @discord.ui.button(label="Broken Rat", emoji="🚔", style=discord.ButtonStyle.grey)
-    async def report_broken_rat(self, interaction: discord.Interaction, button: discord.ui.Button):
-        # Respond to suer
-        await interaction.response.send_message(
-            embed=self.bot.embed(
-                title="Thanks for the report!",
-                description="We'll take him out back.. 😵",
-            ),
-            ephemeral=True,
-        )
-        # Send DM to owner
-        owner_id = self.bot.owner_id
-        assert owner_id
-        owner = self.bot.get_user(owner_id)
-        assert owner
-        await owner.send(
-            embed=self.bot.embed(title="Broken rat 🐀", description=self.rat).set_footer(
-                text=f"Reported by {interaction.user.name} (narc)"
-            )
-        )
-        self.is_broken_rat = True
-        alert_channel = discord.Object(id=1171291004175912980, type=discord.TextChannel)
-        self.stop()
-
-
 class Rats(commands.Cog):
     def __init__(self, bot: Goonbot):
         self.bot = bot
         self.rats = cycle_random(pathlib.Path("rats.txt").read_text().splitlines())
+        self.report_rat_ctx_menu = app_commands.ContextMenu(
+            name="Report Rat", callback=self.report_broken_rat
+        )
+        self.bot.tree.add_command(self.report_rat_ctx_menu)
+
+    async def report_broken_rat(
+        self, interaction: discord.Interaction, message: discord.Message
+    ):
+        if any([not message.embeds, not message.author == self.bot.user]):
+            await interaction.response.send_message(
+                "This isn't a rat post.", ephemeral=True
+            )
+        if message.embeds[0].title == "Rat":
+            await interaction.response.send_message("Rat post!")
 
     @app_commands.command(name="rat")
     async def rat(self, interaction: discord.Interaction):
+        """Roll a rat 🐀"""
         next_rat = next(self.rats)
-        report_rat = ReportRat(next_rat, self.bot)
         await interaction.response.send_message(
             embed=self.bot.embed(title="Rat").set_image(url=next_rat),
-            view=report_rat,
         )
-        await report_rat.wait()
-        if report_rat.is_broken_rat:
-            return await interaction.edit_original_response(
-                embed=discord.Embed(title="This rat was broken", color=discord.Color.greyple()),
-                view=None,
-            )
-        return await interaction.edit_original_response(view=None)
 
 
 async def setup(bot):
