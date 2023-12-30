87126e897d3a91b9e3a34027c0e98aab641c7ce8
JoshPaulie (Bexli) <30572280+JoshPaulie@users.noreply.github.com>
2023-12-30 13:16:34 -0600

    Straggler

--- cogs/_league/cmd/summoner.py
+++ cogs/_league/cmd/summoner.py
@@ -1,6 +1,6 @@
 from pulsefire.clients import RiotAPISchema
 
-from text_processing import multiline_string
+from text_processing import join_lines
 
 from ..calculators import calc_winrate
 from ..formatting import fstat
@@ -9,7 +9,7 @@ from ..formatting import fstat
 def league_entry_stats(entry: RiotAPISchema.LolLeagueV4LeagueFullEntry):
     wins = entry["wins"]
     losses = entry["losses"]
-    return multiline_string(
+    return join_lines(
         [
             fstat("Rank", f"{entry['tier'].title()} {entry['rank']} ({entry['leaguePoints']} lp)"),
             fstat("Win/Loss", f"{wins:,d}/{losses:,d} ({calc_winrate(wins, losses)})"),
