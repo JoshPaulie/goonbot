0adee0e6e40ff78691ddadab2b9e374b1301a48e
JoshPaulie (Bexli) <30572280+JoshPaulie@users.noreply.github.com>
2023-12-31 16:12:56 -0600

    Rework: now uses confirmation view, utilizes gallery view

--- cogs/twitter_embeds.py
+++ cogs/twitter_embeds.py
@@ -1,3 +1,4 @@
+from typing import Any, Coroutine
 from urllib.parse import urlparse
 
 import discord
@@ -5,9 +6,10 @@ from discord import app_commands
 from discord.ext import commands
 
 from goonbot import Goonbot
+from text_processing import join_lines
 
 
-def fix_link(url: str) -> str:
+def fix_link(url: str, use_gallery_view: bool = False) -> str:
     parsed_url = urlparse(url)
     netloc = parsed_url.netloc  # ie. website.tld
     if "x" in netloc:
@@ -16,7 +18,39 @@ def fix_link(url: str) -> str:
         fixed_netloc = "fxtwitter.com"
     else:
         raise ValueError(f"The passed url is not from x or twitter: ...{parsed_url.netloc}...")
+    if use_gallery_view:
+        fixed_netloc = "g." + fixed_netloc
     return parsed_url.scheme + "://" + fixed_netloc + parsed_url.path
+
+
+class Confirm(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=8)
+        self.value = None
+        self.gallery_view = False
+
+    def on_timeout(self) -> Coroutine[Any, Any, None]:
+        self.stop()
+        return super().on_timeout()
+
+    @discord.ui.button(label="Replace link", style=discord.ButtonStyle.green)
+    async def replace_link_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.defer()
+        self.value = True
+        self.stop()
+
+    @discord.ui.button(label="Media only", style=discord.ButtonStyle.green)
+    async def replace_link_gallery_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.defer()
+        self.value = True
+        self.gallery_view = True
+        self.stop()
+
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.grey)
+    async def cancel_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.defer()
+        self.value = False
+        self.stop()
 
 
 class TwitterEmbeds(commands.Cog):
@@ -25,15 +59,33 @@ class TwitterEmbeds(commands.Cog):
 
     @commands.Cog.listener("on_message")
     async def embed_twitter_link(self, message: discord.Message):
-        # make sure the message is from twitter
-        if not any(
-            (
-                message.content.startswith(f"https://twitter"),
-                message.content.startswith(f"https://x"),
-            )
-        ):
+        # Do nothing if the message doesn't start with a twitter domain
+        if not any([message.content.startswith(f"https://{domain}") for domain in ["twitter", "x"]]):
             return
-        await message.reply(fix_link(message.content), silent=True, mention_author=False)
+
+        confirmation_view = Confirm()
+        confirmation_message = await message.reply(
+            embed=self.bot.embed(title="Replace this link with a fxTwitter link?").set_footer(
+                text="You can just ignore this, it'll clear a few seconds."
+            ),
+            view=confirmation_view,
+        )
+
+        # Wait for response (or timeout)
+        await confirmation_view.wait()
+        # Clean up confirmation message (it's spammy)
+        await confirmation_message.delete()
+        # If they chose to replace, delete the original message and send a
+        if confirmation_view.value:
+            await message.delete()
+            await message.channel.send(
+                join_lines(
+                    [
+                        f"**{message.author.mention}** shared",
+                        fix_link(message.content, use_gallery_view=confirmation_view.gallery_view),
+                    ]
+                ),
+            )
 
 
 async def setup(bot):
