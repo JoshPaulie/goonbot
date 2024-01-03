53d8e11a0e2931e5644cc7418b80350eb88cc952
joshpaulie <joshpaullee@gmail.com>
2024-01-02 19:22:10 -0600

    betta worda

--- cogs/goon_calendar.py
+++ cogs/goon_calendar.py
@@ -100,7 +100,7 @@ class GoonCalendar(commands.Cog):
             await interaction.channel.send(
                 embed=self.bot.embed(
                     title="Hey, look.",
-                    description=f"It's that weird edge case <@{self.bot.owner_id}> he thought would never be encountered.",
+                    description=f"It's that weird edge case <@{self.bot.owner_id}> thought he'd never be encountered.",
                 ),
             )
 
