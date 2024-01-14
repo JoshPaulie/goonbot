e23194a53500050895b1556fab4d125559bcedf6
JoshPaulie (Bexli) <30572280+JoshPaulie@users.noreply.github.com>
2024-01-14 17:31:17 -0600

    bug fixes, doc additions

--- cache_league_games.py
+++ cache_league_games.py
@@ -21,6 +21,7 @@ from cogs._league.calculators import duration
 from cogs._league.lookups import discord_to_summoner_name
 from keys import Keys
 
+# Cache, middleware, and client setup the same as it is within the bot
 cache = DiskCache("cache")
 riot_cache_middleware = cache_middleware(
     cache,
@@ -43,36 +44,55 @@ riot_client = RiotAPIClient(
 )
 
 
-async def get_match_history(summoner_name: str, count: int):
+async def cache_match_history(summoner_name: str, count: int):
+    """Fetches the match history of the given summoner. The depth is determined by the count parameter.
+    Pulsefire client is configured to cache api calls"""
+    # Output
     print(f"Fetching {summoner_name}'s last {count} matches...")
+    # Start execution timer
     start_time = time.perf_counter()
+
+    # Make API calls
     async with riot_client as client:
+        # Get summoner details for puuid (player universal unique identity)
         summoner = await client.get_lol_summoner_v4_by_name(region="na1", name=summoner_name)
+        # Get the match IDs for each of the matches, this can be done in 1 API call by specifying how many we want
         match_ids = await client.get_lol_match_v5_match_ids_by_puuid(
             region="americas",
             puuid=summoner["puuid"],
-            queries={"start": 0, "count": 1},
+            queries={"start": 0, "count": count},
         )
 
-    async with TaskGroup(asyncio.Semaphore(100)) as tg:
-        for match_id in match_ids:
-            await tg.create_task(client.get_lol_match_v5_match(region="americas", id=match_id))
-    matches: list[RiotAPISchema.LolMatchV5Match] = tg.results()
+        # Create a task for each match, concurrently ask riot for all of them.
+        async with TaskGroup(asyncio.Semaphore(count)) as tg:
+            for match_id in match_ids:
+                await tg.create_task(client.get_lol_match_v5_match(region="americas", id=match_id))
+        # After all of the tasks have compelted, store the results on the task group
+        matches: list[RiotAPISchema.LolMatchV5Match] = tg.results()
 
+    # Stats for output
     end_time = time.perf_counter()
     elapsed_time = round(end_time - start_time, 2)
+
+    # Output
     print(f"Finished fetching {summoner_name}'s {len(matches)} matches. ({elapsed_time}s)")
 
 
 async def main():
-    count = 10
+    # Get match count
+    count = int(input("Enter match history depth: "))
+    # Output stats
     print(f"Fetching last {count} matches for all goons...")
     start_time = time.perf_counter()
-    summoner_names = list(discord_to_summoner_name.values())
-    for summoner_name in summoner_names[:2]:
-        await get_match_history(summoner_name, count)
+
+    # Get match history for each goon
+    for summoner_name in discord_to_summoner_name.values():
+        await cache_match_history(summoner_name, count)
+
+    # Output stats
     end_time = time.perf_counter()
     elapsed_time = round(end_time - start_time, 2)
+    # Output
     print(f"Finished fetching all goon matches. ({elapsed_time}s)")
 
 
