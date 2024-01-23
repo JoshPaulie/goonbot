1f61ce09bfdf7e42f2d4fcffecb2516da06a41b5
JoshPaulie (Bexli) <30572280+JoshPaulie@users.noreply.github.com>
2024-01-23 17:22:48 -0600

    bump version: 6.0.3

--- goonbot.py
+++ goonbot.py
@@ -20,7 +20,7 @@ class Goonbot(commands.Bot):
     BOTTING_TOGETHER = discord.Object(510865274594131968, type=discord.Guild)  # The development server
     owner_id = 177131156028784640  # bexli boy
 
-    VERSION = (6, 0, 2)
+    VERSION = (6, 0, 3)
 
     keys = Keys
 
