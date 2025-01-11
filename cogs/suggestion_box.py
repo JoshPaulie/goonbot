e544d42ff9ee74e247a19f7574ab4c95f607f55e
bex <30572280+JoshPaulie@users.noreply.github.com>
2025-01-10 21:30:44 -0600

    refactor: move DB logic out of modal and add timeout response

--- cogs/suggestion_box.py
+++ cogs/suggestion_box.py
@@ -1,10 +1,8 @@
 import datetime as dt
-import time
 
 import aiosqlite
 import discord
 import humanize
-from dateutil import tz
 from discord import app_commands
 from discord.ext import commands
 
@@ -15,28 +13,20 @@ class SuggestionModal(discord.ui.Modal, title="Suggestion"):
     details = discord.ui.TextInput(label="Suggestion details", style=discord.TextStyle.paragraph)
 
     async def on_submit(self, interaction: discord.Interaction):
-        # Parse user suggestion
-        details = self.details.value
-
-        # Get timestamp
-        timestamp = dt.datetime.now().isoformat()
-
-        # Make entry in database
-        async with aiosqlite.connect(Goonbot.database_path) as db:
-            await db.execute(
-                """
-                INSERT INTO suggestion (id, userID, details, devNotes, timestamp) 
-                VALUES (?, ?, ?, ?, ?)
-                """,
-                (interaction.id, interaction.user.id, details, None, timestamp),
-            )
-            await db.commit()
-
         # Send confirmation
         await interaction.response.send_message(
             embed=Goonbot.embed(
                 title=f"Thanks for your suggestion!",
-                description="Suggestion has been added to the database and Josh has been notified.",
+                description="Suggestion has been added to the database and admin has been notified.",
+            ),
+            ephemeral=True,
+        )
+
+    async def on_timeout(self, interaction: discord.Interaction) -> None:
+        await interaction.response.send_message(
+            embed=Goonbot.embed(
+                title="You took too long!",
+                color=discord.Color.brand_red(),
             ),
             ephemeral=True,
         )
@@ -45,9 +35,6 @@ class SuggestionModal(discord.ui.Modal, title="Suggestion"):
 class SuggestionBox(commands.Cog):
     def __init__(self, bot: Goonbot):
         self.bot = bot
-
-        # Get precise timestamp for uptime
-        self.startup_time = time.perf_counter()
 
     async def cog_load(self):
         await self.ensure_suggestion_table()
@@ -71,9 +58,29 @@ class SuggestionBox(commands.Cog):
 
     @app_commands.command(name="suggest", description="Suggest a feature or improvement")
     async def suggest(self, interaction: discord.Interaction):
+        # Present modal
         suggestion_modal = SuggestionModal()
         await interaction.response.send_modal(suggestion_modal)
+
+        # Wait for modal response or timeout
         await suggestion_modal.wait()
+
+        # Parse user suggestion
+        details = suggestion_modal.details.value
+
+        # Get timestamp
+        timestamp = dt.datetime.now().isoformat()
+
+        # Make entry in database
+        async with aiosqlite.connect(Goonbot.database_path) as db:
+            await db.execute(
+                """
+                INSERT INTO suggestion (id, userID, details, devNotes, timestamp) 
+                VALUES (?, ?, ?, ?, ?)
+                """,
+                (interaction.id, interaction.user.id, details, None, timestamp),
+            )
+            await db.commit()
 
         # Alert owner of new suggestion
         assert self.bot.owner_id
